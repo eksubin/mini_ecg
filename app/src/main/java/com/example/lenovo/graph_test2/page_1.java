0e6f3fcdf3fa5edb8ad0ec433b38842a8b56eab9
eksubin <subinedu@gmail.com>
2016-07-22 17:10:41 +0530

    all

---
+++ app/src/main/java/com/example/lenovo/graph_test2/page_1.java
@@ -0,0 +1,22 @@
+package com.example.lenovo.graph_test2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+
+
+public class page_1 extends Activity {
+
+    int value;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setContentView(R.layout.activity_page_1);
+    }
+
+}
